ec62adf54cc395e7a81a019845bdb21ab3c4c6aa
AshleyAslihanGoluoglu <119933910+AshleyAslihanGoluoglu@users.noreply.github.com>
2024-05-12 19:44:12 +0300

    uncommenting tests

---
+++ source/tests/test_main.py
@@ -0,0 +1,19 @@
+# from http import HTTPStatus
+import source.main as main
+
+
+TEST_CLIENT = main.app.test_client()
+COMMENTS = "/api/comments"
+TASKS = "/api/tasks"
+
+
+def test_get_comments():
+    resp_json = TEST_CLIENT.get(COMMENTS).get_json()
+    assert isinstance(resp_json, dict)
+    assert len(resp_json) > 0
+
+
+def test_get_tasks():
+    resp_json = TEST_CLIENT.get(TASKS).get_json()
+    assert isinstance(resp_json, dict)
+    assert len(resp_json) > 0
